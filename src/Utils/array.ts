0f949a58bacf33c468f2a7acef447154597fdc51
TheNonStopGamer <rickopdekop@gmail.com>
2021-02-22 22:11:22 +0100

    Apparently previous commit didn't work

---
+++ src/Utils/array.ts
@@ -0,0 +1,4 @@
+import { random_engine_base } from '../Engines/random_engine_base.js';
+import { uniform_int_distribution } from '../Distributions/uniform_int_distribution';
+
+export function index()
