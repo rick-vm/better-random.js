27ce6447bde1fe567aa7a983800fddb9b751f8db
TheNonStopGamer <rickopdekop@gmail.com>
2021-02-23 12:16:11 +0100

    Created most of EMap and iterable

--- src/Utils/iterable.ts
+++ src/Utils/iterable.ts
@@ -1,3 +1,18 @@
-export function entry<T extends Array | Map | Set>(iterable: T): keyof T {
-  return; // retur value
+export function value<T extends V[] | Map<unknown, V> | Set<V>, V>(iterable: T): V | undefined {
+  if (iterable instanceof Array) {
+    return iterable[Math.floor(Math.random() * iterable.length)];
+  } else if (iterable instanceof Map) {
+    return [...iterable.values()][Math.floor(Math.random() * iterable.size)];
+  } else if (iterable instanceof Set) {
+    return [...iterable.values()][Math.floor(Math.random() * iterable.size)];
+  }
+  return;
+}
+
+export function index(iterable: Array<unknown>): number {
+  return Math.floor(Math.random() * iterable.length);
+}
+
+export function key<K>(map: Map<K, unknown>): K | undefined {
+  return [...map.keys()][Math.floor(Math.random() * map.size)];
 }
