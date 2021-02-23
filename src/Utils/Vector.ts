658821837e07bbf85daad62a233228a4154de388
TheNonStopGamer <rickopdekop@gmail.com>
2021-02-23 12:17:56 +0100

    Fixed typing on Vector

--- src/Utils/Vector.ts
+++ src/Utils/Vector.ts
@@ -2,11 +2,11 @@ export class Vector<T> extends Array<T> {
   constructor(size = 0) {
     super(size);
   }
-  
-  public random(): T {
+
+  public random(): T | undefined {
     return this[Math.floor(Math.random() * this.length)];
   }
-  
+
   public randomIndex(): number {
     return Math.floor(Math.random() * this.length);
   }
