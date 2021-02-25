50fc8c94aa03b6936f1dd4fa7cb43734505efaae
TheNonStopGamer <rickopdekop@gmail.com>
2021-02-26 00:32:53 +0100

    yeeted out the caching version because it sucked

--- src/Utils/string.ts
+++ src/Utils/string.ts
@@ -1,5 +1,4 @@
 import { random_engine } from '../Engines/random_engine.js';
-import { EMap } from './EMap.js';
 import { ESet } from './ESet.js';
 
 /**
@@ -41,8 +40,6 @@ export interface UniqueStringGeneratorOptions {
  * 
  * If generating a unique string fails, it returns any random string that fits within the current cycle, this way strings should be uniformly generated
  * 
- * WARNING: BAD PERFORMANCE COMPARED TO OTHER AFTER INITIAL CYCLE, IT IS RECOMMENDED TO USE optimized_unique_string_generator INSTEAD
- * 
  * @param charset The characters to be used in generating the random process
  * @param charCount The amount of times a character is added to (or string if the supplied charset is an array) the generated string, if every string/character is of length 1 this represents the length of the generated string
  * @param uniquePercentage Number between 0 and 1, the percentage of unique strings out of all possible unique strings that at least have to be generated before repeating once. Higher values mean longer generation times but less unique values
@@ -61,39 +58,5 @@ export function unique_string_generator(charset: string | string[], charCount: n
 		if (strings.size >= min_unique) strings.clear();
 		strings.add(str);
 		return str;
-	};
-}
-
-/**
- * Creates a fast unique random string generator, this doesn't generate all possible values (generates the amount of possible unique strings * uniquePercentage) but will be way more performant after first cycle of caching
- * 
- * Caches all results for fast generation times after initial caching cycle
- * If generating a unique string fails, it returns any random string that has been cached, after first caching will return very non-uniform results
- * 
- * WARNING: AFTER FIRST CACHE VERY NON-UNIFORM RESULTS WILL BE RETURNED, IT IS ADVISED TO USE opt_fast_unique_string_generator
- * 
- * @param charset The characters to be used in generating the random process
- * @param charCount The amount of times a character is added to (or string if the supplied charset is an array) the generated string, if every string/character is of length 1 this represents the length of the generated string
- * @param uniquePercentage Number between 0 and 1, the percentage of unique strings out of all possible unique strings that at least have to be generated before repeating once. Higher values mean longer generation times but less unique values
- * @param initialStrings The unique strings the generator should be preloaded with, useful if the strings were previously generated and you need to continue generating unique strings
- * @returns Function that returns a random string, takes a random number generator
- */
-export function fast_unique_string_generator(charset: string | string[], charCount: number, { uniquePercentage = 0.99, initialStrings = undefined }: UniqueStringGeneratorOptions = { uniquePercentage: 0.99, initialStrings: undefined }): (rng: random_engine) => string {
-	const strings = new ESet<string>(initialStrings);
-	let cached = false;
-	const min_unique = charset.length ** charCount * uniquePercentage;
-	return function unique_string(rng: random_engine): string {
-		if (!cached) {
-			let str = '';
-			do {
-				str = '';
-				for (let i = 0; i < charCount; ++i) str += charset[Math.floor(rng.next() / rng.RANGE * charset.length)];
-			} while (strings.has(str) && strings.size < min_unique);
-			cached = strings.size >= min_unique;
-			strings.add(str);
-			return str;
-		} else {
-			return strings.random()!;
-		}
 	};
 }
--- test/index.ts
+++ test/index.ts
@@ -2,7 +2,7 @@ import OutputFile from './OutputFile.js';
 import { xorshift128plus } from '../src/Engines/xorshift128plus.js';
 import { fast_unique_string_generator } from '../src/Utils/string.js';
 
-const of = new OutputFile('./output.txt');
+const of = new OutputFile('./output.txt', { log: false });
 
 const rng = new xorshift128plus();
 const unique_string = fast_unique_string_generator('0123456789', 2, { uniquePercentage: 1 });
@@ -10,9 +10,9 @@ const unique_string = fast_unique_string_generator('0123456789', 2, { uniquePerc
 const arr: string[] = [];
 
 console.time();
-for (let i = 0; i < 10000000; ++i) {
-	arr.push(unique_string(rng));
+for (let i = 0; i < 20000000; ++i) {
+	unique_string(rng);
 }
 console.timeEnd();
-console.log(arr);
-of.analyze(arr, { occurrences: true, duplicates: true });
+
+// of.analyze(arr, { occurrences: true, duplicates: true });
