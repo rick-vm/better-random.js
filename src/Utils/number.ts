b83533f8c4d727e8e1b05697ade7a0c3113c3c5b
TheNonStopGamer <rickopdekop@gmail.com>
2021-02-26 00:33:35 +0100

    yes

--- src/Utils/number.ts
+++ src/Utils/number.ts
@@ -6,7 +6,15 @@ export function unique_int_generator(length: number): (rng: random_engine) => nu
  */
 export function unique_int_generator(min: number, max: number): (rng: random_engine) => number;
 export function unique_int_generator(minOrLength: number, max?: number): (rng: random_engine) => number {
-	return function (rng: random_engine): number {
-		return 0;
-	};
+	if (max) {
+		const range = max - min;
+		return function unique_int(rng: random_engine): number {
+			return Math.floor((rng.next() / rng.RANGE * range) + min);
+		};
+	} else {
+		return function unique_int(rng: random_engine): number {
+
+		}
+	}
+
 }
