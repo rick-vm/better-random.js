0e6980ed305090615060121e3aa0adffd99ee167
TheNonStopGamer <rickopdekop@gmail.com>
2021-02-26 12:47:14 +0100

    Added random int support

--- src/Utils/number.ts
+++ src/Utils/number.ts
@@ -1,15 +1,19 @@
 import { random_engine } from '../Engines/random_engine.js';
+import { ESet } from '../Engines/random_enginr.js';
 
 export function unique_int_generator(length: number): (rng: random_engine) => number;
-/**
- * @param min 
- */
 export function unique_int_generator(min: number, max: number): (rng: random_engine) => number;
 export function unique_int_generator(minOrLength: number, max?: number): (rng: random_engine) => number {
 	if (max) {
-		const range = max - min;
+	  const numbers = new ESet<number>();
+	  const range = max - minOrLength;
 		return function unique_int(rng: random_engine): number {
-			return Math.floor((rng.next() / rng.RANGE * range) + min);
+		  let num: number;
+		  do {
+		    num = Math.floor((rng.next() / rng.RANGE * range) + minOrLength);
+		  } while (numbers.has(num));
+		  numbers.add(num);
+			return num;
 		};
 	} else {
 		return function unique_int(rng: random_engine): number {
